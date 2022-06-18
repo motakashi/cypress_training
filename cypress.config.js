ae2d2bb5f0a0dc5a704818a5604441628f8c08c6
motakashi <takashi_home_sky@yahoo.co.jp>
2022-06-18 19:18:51 +0900

    設定ファイルの変更

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+// Populate process.env with values from .env file
+require('dotenv').config()
+
+module.exports = defineConfig({
+  userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1",
+  env: {
+    YID: process.env.YID,
+    PASSWORD: process.env.PASSWORD
+  }
+})
