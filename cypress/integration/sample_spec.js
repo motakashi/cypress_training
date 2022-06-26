26158f05687369551ed2aaf7dfc1177038e6ebbd
motakashi <takashi_home_sky@yahoo.co.jp>
2022-06-26 21:35:46 +0900

    ログイン処理をbeforeEachに追加

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,38 +1,29 @@
 describe('トップページのテスト', () => {
   it('Visits the Kitchen Sink', () => {
-
     cy.viewport("iphone-6");
-
     cy.visit('https://donation.yahoo.co.jp/');
-
     cy.get('#search > form > div > input').type('領収書');
-
     cy.get('#search > form > div > button').click();
-
     cy.url().should('include', '/search');
-
     cy.get('#result > h1')
       .should('have.text', '検索結果')
-  
     cy.screenshot('test', {capture:'fullPage'});
   })
 })
 
 describe('Tポイントのテスト', () => {
 
-  it('ログイン画面が表示されログインができるか', () => {
+  beforeEach(() => {
     cy.viewport("iphone-6");
-    cy.visit('https://donation.yahoo.co.jp/detail/1630001/point');
-    cy.get('#username').type(Cypress.env('YID'));
-    cy.get('#btnNext').click();
-    cy.get('#passwd').type(Cypress.env('PASSWORD'));
-    cy.get('#btnSubmit').click();
-
+    // 一度だけログインすることで、各テスト項目にsessionを引き継ぐことができる
+    cy.login(Cypress.env('YID'), Cypress.env('PASSWORD'));
     cy.visit('https://donation.yahoo.co.jp/detail/1630001/point');
     cy.url().should('eq', 'https://donation.yahoo.co.jp/detail/1630001/point');
+  })
 
+  it('ログイン画面が表示されログインができるか', () => {
     const beforePoint = cy.get('#tabbox > section > h2 > span.number');
-    //cy.log(beforePoint.invoke('text'));
-    beforePoint.should('have.text', '0')
+    cy.log(beforePoint.invoke('text'));
+    //beforePoint.should('have.text', '0');
   })
 })
